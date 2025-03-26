03cf0eb9fa2ad1c461d97f14b3fddcc87307bb25
Ken Dunlap <kdunlap4918@gmail.com>
2025-03-26 15:06:10 -0400

    feat: added nginx config to deploy

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -2,7 +2,7 @@
 
 # make sure you've copied the default file from the nginx site-enabled and edited the root directory to 
 #/home/site/wwwroot/public 
-cp /home/site/default /etc/nginx/sites-available/default
+cp home/site/wwwroot/deploy/nginx-config /etc/nginx/sites-available/default
 service nginx reload
 
 cd /home/site/wwwroot && cp -n .env.example .env 
