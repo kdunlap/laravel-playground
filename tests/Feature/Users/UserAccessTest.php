350f65ec5411e1305bf328625c9ffd4ce0b2b707
Ken Dunlap <kdunlap4918@gmail.com>
2025-03-26 10:57:21 -0400

    fix: updated test to fix ci test runner, removed linting job

--- tests/Feature/Users/UserAccessTest.php
+++ tests/Feature/Users/UserAccessTest.php
@@ -6,15 +6,38 @@ use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserAccessTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_guests_are_unable_to_access_the_users_page()
+    public function test_guests_are_redirected_to_the_login_page()
     {
+        $response = $this->get('/users');
+        $response->assertStatus(302);
+        $response->assertLocation('http://localhost/login');
+    }
+
+    public function test_users_without_role_viewAny_cannot_visit_the_users_page()
+    {
+        $user = User::factory()
+            ->has(
+                Role::factory()
+                    ->has(Permission::factory()
+                        ->state([
+                            // some fake permission
+                            'name' => 'Can edit themselves', 
+                            'slug' => 'user_self'
+                        ]))
+                    ->state([
+                        'name' => 'User', 
+                        'slug' => 'user' 
+                    ])
+            )
+            ->create();
+        $this->actingAs($user);
+
         $response = $this->get('/users');
         $response->assertStatus(403);
     }
