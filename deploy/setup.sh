9aad7339ffab3bac14762f72264514c87e8b04de
Ken Dunlap <kdunlap4918@gmail.com>
2025-03-26 21:42:42 -0400

    fix: correcting deployment issues

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -2,8 +2,11 @@
 
 # make sure you've copied the default file from the nginx site-enabled and edited the root directory to 
 #/home/site/wwwroot/public 
-cp home/site/wwwroot/deploy/nginx-config /etc/nginx/sites-available/default
+cp /home/site/wwwroot/deploy/nginx-config /etc/nginx/sites-available/default
 service nginx reload
 
 cd /home/site/wwwroot && cp -n .env.example .env 
 php artisan key:generate
+
+touch database/database.sqlite
+php artisan migrate --seed
