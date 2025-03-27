de23896fcb5e5788b0c6a3d08de6574afd39d2ec
Ken Dunlap <kdunlap4918@gmail.com>
2025-03-27 09:32:01 -0400

    feat: added config clear and forcing https when in production

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,8 +5,11 @@ cp /etc/nginx/sites-enabled/default /home/site/ext
 cp /home/site/wwwroot/deploy/nginx-config /etc/nginx/sites-available/default
 service nginx reload
 
-cd /home/site/wwwroot && cp -n .env.example .env 
+cd /home/site/wwwroot && cp -n .env.production .env 
 php artisan key:generate
 
 touch database/database.sqlite
 php artisan migrate --force
+
+php artisan config:clear
+php artisan config:cache
