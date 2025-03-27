07b4ea78c51b396ffa8912ec41739aecc37ae939
Ken Dunlap <kdunlap4918@gmail.com>
2025-03-27 11:05:49 -0400

    feat: added second column to bottom row of Dashboard page

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -28,8 +28,14 @@ const breadcrumbs: BreadcrumbItem[] = [
                     <PlaceholderPattern />
                 </div>
             </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
-                <PlaceholderPattern />
+
+            <div class="grid auto-rows-min gap-4 md:grid-cols-2">
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <PlaceholderPattern />
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <PlaceholderPattern />
+                </div>
             </div>
         </div>
     </AppLayout>
